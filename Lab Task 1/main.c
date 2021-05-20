9cd1b04eb7351d5f9d8ec7d8dab31f88df788d9c
Mohammad Abuzar <abuzarmohammad945@gmail.com>
2021-05-20 00:48:52 -0700

    Add files via upload

---
+++ Lab Task 1/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include"myheader.h"
+
+int main(void)
+{
+
+printf("Name:  Mohammad Abuzar \n");
+printf("SID:   SP19-BCS-089 \n");
+printf("Age:    %d\n",Age);
+
+return 1;
+
+}
---
+++ Lab Task 1/myheader.h
@@ -0,0 +1,1 @@
+#define Age 20
